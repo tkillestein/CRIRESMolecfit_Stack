3b24d68c70cbdeeb6fb90071391c6a08038c973b
tkillestein <thomas.killestein@gmail.com>
2019-08-14 10:47:08 +0100

    Telluric fetching implemented

--- process_frames.py
+++ process_frames.py
@@ -1,9 +1,10 @@
-import os, glob
+import os, glob, shutil
+from astropy.io import fits
 
 from cal_selector import grab_calfiles
 from crires_pipeline_bootstrapper import calibrate_frames
-
-
+from skycalc_handler import grab_tellurics
+from utils import mkdir_safe
 ######## Handle calibration files
 BASEPATH = os.getcwd()
 
@@ -13,7 +14,25 @@ print(folders)
 
 for f in folders:
     os.chdir(f)
-    grab_calfiles()
-    calibrate_frames()
+    #grab_calfiles()
+    #calibrate_frames()
+
+    ### Read FITS header from crires_spec_jitter_extracted.FITS
+    temphdu = fits.open("obj/crires_spec_jitter_extracted.fits")
+    header = temphdu[0].header
+    temphdu.close()
+    ### Pass this to the telluric grabber
+    grab_tellurics(header)
+    ### Run wavelength alignment
+
+    ### Copy processed file to proc/
+    proc_path = os.path.join(BASEPATH, "proc", f[4:])
+    mkdir_safe(proc_path)
+    shutil.copy("obj/crires_spec_jitter_extracted.fits", proc_path + "/%s%s" % (f[4:], ".fits"))
+
     os.chdir(BASEPATH)
     print("Folder %s of %s complete" % (folders.index(f) + 1, len(folders)))
+
+
+    #### Run telluric grabber for the spectrum
+    #### Add in wavelength calibration
--- skycalc_handler.py
+++ skycalc_handler.py
@@ -14,51 +14,35 @@ Created on Tue Aug  6 10:31:02 2019
 import os, glob
 from astropy.io import fits
 
+def grab_tellurics(fitsheader):
 
-filename = "test_clean.fits"
-
-#### make skycalc_temp directory
-if os.path.isdir("skycalc_temp") == True:
-    flist = glob.glob("skycalc_temp/*")
-    for f in flist:
-        os.remove(f)
-else:
-    os.mkdir("skycalc_temp")
-
-
-### Read in key parameters from ESO fits files
-hdu = fits.open(filename)
-header = hdu[0].header
-hdu.close()
-
-### almanac, need ra, dec, observatory, timestamp
-ra = hdu[0].header["RA"]
-dec = hdu[0].header["DEC"]
-obs = "paranal"
-date = hdu[0].header["DATE"]
-
-alm_input = open("skycalc_temp/input_almanac.txt", "w+")
-alm_input.write("ra : " + str(ra) + "\n")
-alm_input.write("dec : " + str(dec) + "\n")
-alm_input.write("observatory : " + obs + "\n")
-alm_input.write("date : " + str(date) + "\n")
-alm_input.close()
+    #### make skycalc_temp directory
+    if os.path.isdir("skycalc_temp") == True:
+        flist = glob.glob("skycalc_temp/*")
+        for f in flist:
+            os.remove(f)
+    else:
+        os.mkdir("skycalc_temp")
 
 
-#### Now generate files across the detector range
-sk_input = open("skycalc_temp/input_skycalc.txt", "w+") 
-wmin = header["HIERARCH ESO INS WLEN STRT1"] - 5
-wmax = header["HIERARCH ESO INS WLEN END4"] + 5
-res = (wmax - wmin) / 4096
+    ### Read in key parameters from ESO fits files
+    ### almanac, need ra, dec, observatory, timestamp
+    ra = fitsheader["RA"]
+    dec = fitsheader["DEC"]
+    date = fitsheader["DATE"]
+    airm = 0.5*(fitsheader["HIERARCH ESO TEL AIRM END"] + fitsheader["HIERARCH ESO TEL AIRM END"])
+    wmin = fitsheader["HIERARCH ESO INS WLEN STRT1"] - 5 # 5 nm buffer to allow for jitter.
+    wmax = fitsheader["HIERARCH ESO INS WLEN END4"] + 5
+    res = 200000
 
-sk_input.write("wmin : " + str(wmin) + "\n")
-sk_input.write("wmax : " + str(wmax) + "\n")
-sk_input.write("wdelta: " + str(res) + "\n")
-sk_input.write("msolflux: 130.0" +"\n") # implicitly set the default value, so almanac doesn't make it NaN
-sk_input.close()
+    #### Now generate files across the detector range
+    sk_input = open("skycalc_temp/input_skycalc.txt", "w+")
+    sk_input.write("observatory : paranal \n")
+    sk_input.write("wmin : %s \n" % (np.floor(wmin)))
+    sk_input.write("wmax : %s \n" % (np.ceil(wmax)))
+    sk_input.write("wres : %s \n" % (res))
+    sk_input.write("airmass : %s \n" % (airm))
+    sk_input.close()
 
-
-cmd = "skycalc_cli" + " -a skycalc_temp/input_almanac.txt" + " -o output_tellur.fits" + " -i skycalc_temp/input_skycalc.txt"
-
-os.system(cmd)
-
+    cmd = "skycalc_cli" + " -o telluric_model.fits" + " -i skycalc_temp/input_skycalc.txt"
+    os.system(cmd)
